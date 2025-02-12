827b5dc2b1aaa86bfbdc8ed32112f95dab376599
TuanSave98 <tuantaitu98@gmail.com>
2025-02-12 08:56:56 +0700

    update

---
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>LuuVan</title>
+  <base href="/LuuVan/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-RLC5FR6P.js" defer=""></script><script src="main-PATAMNT2.js" type="module"></script></body>
+</html>
+`;

3186ca3962ad2414f267cda9fb75b891429e7fb5
TuanSave98 <tuantaitu98@gmail.com>
2025-02-12 08:50:44 +0700

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 562, hash: '84ec4107e68fe6bb493e4fd210a566d937779e4d1ad4c1fad071466a01989f8b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1075, hash: '488b0b0ad24f095e40d551718dc4cac5d9bc47ec218caab5903aa1d626aa380f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 562, hash: '759c182b4c078a846497e810005eda1650568b5ff8618992d3d24105cba0931a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1075, hash: 'd6b802d2d22f4ee15cb5060ccebb2a63b1fc74077829bbed46fcfe6a719afa8b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
